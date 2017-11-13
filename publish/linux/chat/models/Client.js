b90c8d644c989a9455cd8b183cbf5031b831b283
furkan dogan <dogan.furkan@msn.com>
2017-11-13 19:22:00 +0300

    publish ignore

--- publish/linux/chat/models/Client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Client {
-    constructor(connectionId, dhMemberId) {
-        this.ConnectionId = connectionId;
-        this.DhMemberId = dhMemberId;
-    }
-}
-exports.Client = Client;
